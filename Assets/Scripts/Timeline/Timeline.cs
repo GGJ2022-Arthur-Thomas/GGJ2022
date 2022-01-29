00b59595a8ba10e59e90ea11aa2b0ee94956d7bc
Arthur Cousseau <arthur.cousseau@me.com>
2022-01-29 19:37:16 +0100

    Added NewGodRequests on Timeline

--- Assets/Scripts/Timeline/Timeline.cs
+++ Assets/Scripts/Timeline/Timeline.cs
@@ -1,14 +1,40 @@
 using UnityEngine;
 using ExtensionMethods;
 using UnityEngine.UI;
+using System.Linq;
+using System.Collections.Generic;
 
 public class Timeline : Singleton<Timeline>
 {
+    [Header("Progress bar")]
+
     [SerializeField] private RectTransform progressBarTransform;
     [SerializeField] private RectTransform fillTransform;
+
+    [Header("Days grid")]
+
     [SerializeField] private GridLayoutGroup daysGridLayoutGroup;
     [SerializeField] private GameObject dayPrefab;
 
+    [Header("God requests")]
+
+    [SerializeField] private GameObject godRequestPrefab;
+    [SerializeField] private Transform godRequestsParent;
+    [SerializeField]
+    [Min(0)]
+    private int minGodRequests;
+
+    [SerializeField]
+    [Min(0)]
+    private int maxGodRequests;
+
+    [SerializeField]
+    [Min(0)]
+    [Tooltip("In seconds.")]
+    private int minTimeBetweenGodRequests = 30;
+
+    [Header("Day settings")]
+    
     [SerializeField]
     [Min(1)]
     private int nbDays = 40;
@@ -22,6 +48,10 @@ public class Timeline : Singleton<Timeline>
     private bool ended;
     private float totalDuration;
     private float progressBarWidth;
+    private float[] godRequestTimes;
+    private int nextGodRequestIndex;
+    private float NextGodRequestTime => godRequestTimes[nextGodRequestIndex];
+    private bool HasNextGodRequest => nextGodRequestIndex < godRequestTimes.Length;
 
     /// <summary>
     /// Between 0 and <see cref="nbDays"/>.
@@ -56,6 +86,9 @@ public class Timeline : Singleton<Timeline>
         daysGridLayoutGroup.cellSize = new Vector2(progressBarWidth / nbDays, daysGridLayoutGroup.cellSize.y);
 
         SpawnDays();
+        godRequestTimes = CalculateGodRequestTimes();
+        nextGodRequestIndex = 0;
+        SpawnGodRequests();
 
         NotifyNewDay();
     }
@@ -67,36 +100,105 @@ public class Timeline : Singleton<Timeline>
         for (int i = 0; i < nbDays; i++)
         {
             var dayGO = Instantiate(dayPrefab, daysGridTransform);
+            dayGO.name = $"Day_{i}";
             var dayScript = dayGO.GetComponent<DayUI>();
             dayScript.SetDay(i);
         }
     }
 
-    private void Update()
+    private void SpawnGodRequests()
+    {
+        godRequestsParent.ClearChildren();
+
+        for (int i = 0; i < godRequestTimes.Length; i++)
+        {
+            var godRequestGO = Instantiate(godRequestPrefab, godRequestsParent);
+            godRequestGO.name = $"GodRequest_{i}";
+            godRequestGO.GetComponent<Image>().enabled = true; // for a weird reason, Image component gets disabled when spawning
+            godRequestGO.transform.Translate(Vector3.right * ((godRequestTimes[i] / totalDuration) * progressBarWidth - progressBarWidth / 2));
+        }
+    }
+
+    private float[] CalculateGodRequestTimes()
+    {
+        int nbGodRequests = Random.Range(minGodRequests, maxGodRequests);
+        var tmpList = new List<(float, bool)>(); // = (time, takeIntoFinalResult)
+
+        for (int i = 0; i < nbGodRequests; i++)
+        {
+            tmpList.Add((Random.Range(minTimeBetweenGodRequests, totalDuration), true));
+        }
+
+        tmpList = tmpList.OrderBy(x => x.Item1).ToList(); // sort by time
+
+        for (int i = 1; i < nbGodRequests; i++)
+        {
+            if (tmpList[i].Item1 - tmpList[i - 1].Item1 < minTimeBetweenGodRequests)
+            {
+                tmpList[i] = (tmpList[i].Item1, false); // too close to previous, don't take into final result
+            }
+        }
+
+        // don't take into final result on sundays (god is resting)
+        tmpList = tmpList.Where(x => GetDayInWeekNb(x.Item1) != 6).ToList();
+
+        return tmpList.Where(x => x.Item2 == true).Select(x => x.Item1).ToArray();
+    }
+
+    /// <summary>
+    /// NOTE: day in week nb is [0-6]
+    /// </summary>
+    private int GetDayInWeekNb(float time)
+    {
+        return GetDayNb(time) % 7;
+    }
+
+    /// <summary>
+    /// NOTE : day nb is [0-nbDays]
+    /// </summary>
+    private int GetDayNb(float time)
+    {
+        return (int)((time / totalDuration) * nbDays);
+    }
+
+    void Update()
     {
         if (ended)
             return;
 
+        float previousTime = CurrentTime;
         currentDayTime += Time.deltaTime;
 
-        if (currentDayTime >= dayDurationInSeconds)
+        if (HasNextGodRequest &&
+            previousTime <= NextGodRequestTime && CurrentTime > NextGodRequestTime) // just passed a new god request
+        {
+            NotifyNewGodRequest();
+            nextGodRequestIndex++;
+        }
+
+        if (currentDayTime >= dayDurationInSeconds) // just passed a new day
         {
             currentDayTime = 0f;
             currentDayNb++;
 
-            if (currentDayNb == nbDays)
+            if (currentDayNb < nbDays) // regular new day
+            {
+                NotifyNewDay();
+            }
+            else // end of last day
             {
                 this.Publish<TimelineEndedEvent>();
                 ended = true;
                 return;
             }
-            else
-            {
-                NotifyNewDay();
-            }
         }
 
         UpdateFill();
+    }
+
+    private void NotifyNewGodRequest()
+    {
+        this.Publish<NewGodRequestEvent>();
     }
 
     private void NotifyNewDay()
