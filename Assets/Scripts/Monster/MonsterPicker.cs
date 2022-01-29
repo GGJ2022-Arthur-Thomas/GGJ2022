acff6ffe8a3a98cbb66d57602090cbd95fce6fb4
Srykah <srykah@gmail.com>
2022-01-29 19:28:33 +0100

    MonsterPicker handles NewMonsterNeededEvent and publishes NewMonsterArrivesEvent

--- Assets/Scripts/Monster/MonsterPicker.cs
+++ Assets/Scripts/Monster/MonsterPicker.cs
@@ -1,26 +1,66 @@
-using System;
+using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 using Folder;
 
-public sealed class MonsterPicker : Singleton<MonsterPicker>
+public sealed class MonsterPicker : Singleton<MonsterPicker>,
+    IEventHandler<NewMonsterNeededEvent>
 {
     [Folder]
     [SerializeField]
     private string monstersFolder;
     
     private Monster[] monsters;
-    
-    public Monster CurrentMonster { get; private set; }
-    
+
+    private const int MONSTER_QUEUE_SIZE = 5;
+    private const int MONSTER_QUEUE_PICK_MAX_RETRIES = 5;
+
+    public List<Monster> MonsterQueue { get; private set; } = new List<Monster>();
+    public Monster CurrentMonster => MonsterQueue[0];
+
     void Start()
     {
         monsters = monstersFolder.LoadFolder<Monster>();
-        PickRandomMonster();
+        InitMonsterQueue();
         Debug.Log(CurrentMonster.DisplayName);
+        this.Subscribe<NewMonsterNeededEvent>();
     }
 
-    private void PickRandomMonster()
+    protected override void OnDestroy()
     {
-        CurrentMonster = monsters.GetRandomElement();
+        this.UnSubscribe<NewMonsterNeededEvent>();
+        base.OnDestroy();
+    }
+    
+    void IEventHandler<NewMonsterNeededEvent>.Handle(NewMonsterNeededEvent newMonsterNeededEvent)
+    {
+        MoveQueueForward();
+    }
+
+    private void InitMonsterQueue()
+    {
+        foreach (var i in Enumerable.Range(1, MONSTER_QUEUE_SIZE))
+        {
+            AddRandomUniqueMonsterToQueue();
+        }
+    }
+
+    private void MoveQueueForward()
+    {
+        AddRandomUniqueMonsterToQueue();
+        MonsterQueue.RemoveAt(0);
+        Debug.Log("New current monster is: " + CurrentMonster.DisplayName);
+        this.Publish(new NewMonsterArrivesEvent());
+    }
+
+    private void AddRandomUniqueMonsterToQueue()
+    {
+        var pick = monsters.GetRandomElement();
+        for (var i = 1; MonsterQueue.Contains(pick) && i < MONSTER_QUEUE_PICK_MAX_RETRIES; ++i)
+        {
+            pick = monsters.GetRandomElement();
+        }
+        MonsterQueue.Add(pick);
+        Debug.Log("Added " + pick.DisplayName + " at the end of the queue!");
     }
 }
