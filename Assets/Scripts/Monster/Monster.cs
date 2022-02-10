3ba91154fa4367aaa1efdbdd6ebb46b0e2aea1e4
Arthur Cousseau <arthur.cousseau@me.com>
2022-02-10 20:26:54 +0100

    Reformat Monster.cs

--- Assets/Scripts/Monster/Monster.cs
+++ Assets/Scripts/Monster/Monster.cs
@@ -1,15 +1,20 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 [CreateAssetMenu(menuName = "Monster", fileName = "Monster")]
 public class Monster : ScriptableObject
 {
-    [SerializeField] private string displayName;
-    [SerializeField] private Sprite sprite;
-    [SerializeField] [TextArea] private string description;
+    [SerializeField]
+    private string displayName;
+
+    [SerializeField]
+    private Sprite sprite;
+
+    [SerializeField]
+    [TextArea]
+    private string description;
+
     
-    public Sprite Sprite => sprite;
     public string DisplayName => displayName;
+    public Sprite Sprite => sprite;
     public string Description => description;
 }
